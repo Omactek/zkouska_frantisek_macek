a65d0ec764fe323a82bd3fe56764cf3166bcc474
František Macek <frantisek.macek1@gmail.com>
2022-02-11 17:04:44 +0100

    negativni cisla

--- convert_binary_decimal_11/convertor_code.py
+++ convert_binary_decimal_11/convertor_code.py
@@ -1,8 +1,15 @@
-binary = "100101"
-decimal = 0
+binary_input = "111001010"
+binary = ""
+if binary_input[0]=="1":
+    binary = binary_input[1:]
+else:
+    binary = binary_input
 
-
-for i in reversed(range(len(binary))):
-    decimal = int(binary[-(i+1)])*(2**i) + decimal
-print(decimal)
+#function converts binary number to decimal number
+def bin_to_dec(binary_num):
+    decimal = 0
+    for i in reversed(range(len(binary_num))):
+        decimal = int(binary_num[-(i+1)])*(2**i) + decimal
+    return decimal
+print("-"+str(bin_to_dec(binary)))
     
