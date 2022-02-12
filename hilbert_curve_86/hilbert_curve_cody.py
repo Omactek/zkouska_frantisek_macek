44ac1a62d9a664fea2e673d44db17e9dd8567c83
František Macek <frantisek.macek1@gmail.com>
2022-02-12 20:06:20 +0100

    final version

--- hilbert_curve_86/hilbert_curve_cody.py
+++ hilbert_curve_86/hilbert_curve_cody.py
@@ -1,12 +1,24 @@
 from inspect import trace
 import turtle as t
 
-width = 500
-t.speed(8)
-sc = t.Screen()
-sc.setup(width, width)
-distance = 10
+width = 500 #value for the width of turtle window
+window_width = width + width/100 #takes care of turtle window margin
+order = 0 #value for order
 
+#function that asks for user input
+def user_input():
+    while True:
+        try:
+            order_input = int(input("Enter order of Hilbert curve: "))
+            if order_input > 0:
+                break
+            else:
+                print("Please input positive integer.")
+        except ValueError:
+            print("Please enter number.")
+    return order_input
+
+#function draws hilbert curve
 def hilbert_curve(order, angle):
     if order == 0:
         return
@@ -23,8 +35,20 @@ def hilbert_curve(order, angle):
     hilbert_curve(order-1, -angle)
     t.left(angle)
 
-hilbert_curve(2,90)
+t.speed(0)
+sc = t.Screen()
+sc.setup(window_width, window_width) #sets up turtle window to chosen width
 
+order = user_input()
+
+try:
+    distance = width/((2**order)**2/(2**order)-1) #calculates the distance for one instance
+except OverflowError:
+    print("Error: you entered too big number for order")
+    quit()
+
+t.setworldcoordinates(0,0,window_width,window_width) #sets coordinates of right corner to 0
+
+hilbert_curve(order,90) #calls the hilbert function with chosen order
 
 t.exitonclick()
-
